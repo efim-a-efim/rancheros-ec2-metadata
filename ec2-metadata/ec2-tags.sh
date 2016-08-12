9f1653d262ba47f99d99cad609c36e4723559938
Alexander Efimov <alexander.efimov@dataart.com>
2016-08-12 15:29:31 +0300

    added empty filter support

--- ec2-metadata/ec2-tags.sh
+++ ec2-metadata/ec2-tags.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 INSTANCE="${1}"
-TAG="${2:-com.}"
+TAG_FILTER=''
+[ "$2" ] && TAG_FILTER="| select(.Key|startswith(\"${2}\"))"
 OPTS=$(ros config get rancher.docker.args | grep -v '^\s*$' | grep -v '^$' | while read m opt; do echo -n "'${opt}',"; done)
-AWS_OPTS=$(aws ec2 describe-tags --region "${AWS_DEFAULT_REGION}" --filters "Name=resource-id,Values=${INSTANCE}" 2>/dev/null | jq -r -j ".Tags[] | select(.Key|startswith(\"${TAG}\")) | \" '--label','\(.Key)=\(.Value)',\"" 2>/dev/null)
+AWS_OPTS=$(aws ec2 describe-tags --region "${AWS_DEFAULT_REGION}" --filters "Name=resource-id,Values=${INSTANCE}" 2>/dev/null | jq -r -j ".Tags[] ${TAG_FILTER} | \" '--label','\(.Key)=\(.Value)',\"" 2>/dev/null)
 ros config set rancher.docker.args "[ ${OPTS%?}, ${AWS_OPTS%?} ]"
