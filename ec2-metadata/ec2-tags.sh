ca47b6ff48519cf951b9e30b3c24b3b55069f55d
Alexander Efimov <alexander.efimov@dataart.com>
2016-08-15 13:23:30 +0300

    Changed config set method

--- ec2-metadata/ec2-tags.sh
+++ ec2-metadata/ec2-tags.sh
@@ -2,6 +2,17 @@
 INSTANCE="${1}"
 TAG_FILTER=''
 [ "$2" ] && TAG_FILTER="| select(.Key|startswith(\"${2}\"))"
-OPTS=$(ros config get rancher.docker.args | grep -v '^\s*$' | grep -v '^$' | while read m opt; do echo -n "'${opt}',"; done)
-AWS_OPTS=$(aws ec2 describe-tags --region "${AWS_DEFAULT_REGION}" --filters "Name=resource-id,Values=${INSTANCE}" 2>/dev/null | jq -r -j ".Tags[] ${TAG_FILTER} | \" '--label','\(.Key)=\(.Value)',\"" 2>/dev/null)
-ros config set rancher.docker.args "[ ${OPTS%?}, ${AWS_OPTS%?} ]"
+
+echo "#cloud-config"
+echo "rancher:"
+echo "  docker:"
+echo "    args:"
+
+ros config get rancher.docker.args | grep -v '\[\]' | grep -v '^\s*$'
+
+aws ec2 describe-tags --region "${AWS_DEFAULT_REGION}" --filters "Name=resource-id,Values=${INSTANCE}" 2>/dev/null \
+  | jq -r -j ".Tags[] ${TAG_FILTER} | \"\(.Key)=\(.Value)\"" 2>/dev/null \
+  | while read label; do
+    echo "      - --label"
+    echo "      - ${label}"
+  done
--- entry.sh
+++ entry.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 LIB_PATH="/lib/ec2-metadata"
+CONFIG_PATH="/var/lib/rancher/conf/cloud-config.d"
+
+echo '#cloud-config' > "${NEW_CONFIG}"
+echo "rancher:" >> "${NEW_CONFIG}"
 
 # Basic metadata
-export AWS_AVAILABILITY_ZONE=`wget -O- -q http://169.254.169.254/latest/meta-data/placement/availability-zone 2>/dev/null`
+export AWS_AVAILABILITY_ZONE="$(wget -O- -q http://169.254.169.254/latest/meta-data/placement/availability-zone 2>/dev/null)"
 export AWS_DEFAULT_REGION="${AWS_AVAILABILITY_ZONE%?}"
-export AWS_IAM_ROLE=`wget -O- -q "http://169.254.169.254/latest/meta-data/iam/info" 2>/dev/null | grep 'InstanceProfileArn' | tr -d ' ",' | cut -d '/' -f 2`
+export AWS_IAM_ROLE="$(wget -O- -q "http://169.254.169.254/latest/meta-data/iam/info" 2>/dev/null | grep 'InstanceProfileArn' | tr -d ' ",' | cut -d '/' -f 2)"
 if [ "${AWS_IAM_ROLE}" ]; then
-  export AWS_ACCESS_KEY_ID=`wget -O- -q "http://169.254.169.254/latest/meta-data/iam/security-credentials/${AWS_IAM_ROLE}" 2>/dev/null | grep 'AccessKeyId' | tr -d ' ",' | cut -d ':' -f 2`
-  export AWS_SECRET_ACCESS_KEY=`wget -O- -q "http://169.254.169.254/latest/meta-data/iam/security-credentials/${AWS_IAM_ROLE}" 2>/dev/null | grep 'SecretAccessKey' | tr -d ' ",' | cut -d ':' -f 2`
-  export AWS_SECURITY_TOKEN=`wget -O- -q "http://169.254.169.254/latest/meta-data/iam/security-credentials/${AWS_IAM_ROLE}" 2>/dev/null | grep 'Token' | tr -d ' ",' | cut -d ':' -f 2`
+  export AWS_ACCESS_KEY_ID="$(wget -O- -q "http://169.254.169.254/latest/meta-data/iam/security-credentials/${AWS_IAM_ROLE}" 2>/dev/null | grep 'AccessKeyId' | tr -d ' ",' | cut -d ':' -f 2)"
+  export AWS_SECRET_ACCESS_KEY="$(wget -O- -q "http://169.254.169.254/latest/meta-data/iam/security-credentials/${AWS_IAM_ROLE}" 2>/dev/null | grep 'SecretAccessKey' | tr -d ' ",' | cut -d ':' -f 2)"
+  export AWS_SECURITY_TOKEN="$(wget -O- -q "http://169.254.169.254/latest/meta-data/iam/security-credentials/${AWS_IAM_ROLE}" 2>/dev/null | grep 'Token' | tr -d ' ",' | cut -d ':' -f 2)"
 fi
-export AWS_INSTANCE_ID=`wget -O- -q "http://169.254.169.254/latest/meta-data/instance-id" 2>/dev/null`
+export AWS_INSTANCE_ID="$(wget -O- -q "http://169.254.169.254/latest/meta-data/instance-id" 2>/dev/null)"
 
 while getopts ':t:l:m' opt; do
   case "$opt" in
     t)
-      bash "${LIB_PATH}/ec2-tags.sh" "${AWS_INSTANCE_ID}" "${OPTARG}"
+      NEW_CONFIG=`mktemp -p "${CONFIG_PATH}"`
+      bash "${LIB_PATH}/ec2-tags.sh" "${AWS_INSTANCE_ID}" "${OPTARG}" | sed 's/^/  /' >> "${NEW_CONFIG}"
+      bash "${LIB_PATH}/set-config.sh" < "${NEW_CONFIG}"
+      rm "${NEW_CONFIG}"
       ;;
     m)
-      bash "${LIB_PATH}/ec2-metadata.sh" "rancher.environment"
+      NEW_CONFIG=`mktemp -p "${CONFIG_PATH}"`
+      bash "${LIB_PATH}/ec2-metadata.sh" | sed 's/^/  /' >> "${NEW_CONFIG}"
+      bash "${LIB_PATH}/set-config.sh" < "${NEW_CONFIG}"
+      rm "${NEW_CONFIG}"
       ;;
     l)
-      bash "${LIB_PATH}/ec2-label-to-env.sh" "${AWS_INSTANCE_ID}" "${OPTARG}"
+      NEW_CONFIG=`mktemp -p "${CONFIG_PATH}"`
+      bash "${LIB_PATH}/ec2-label-to-env.sh" "${AWS_INSTANCE_ID}" "${OPTARG}" | sed 's/^/  /' >> "${NEW_CONFIG}"
+      bash "${LIB_PATH}/set-config.sh" < "${NEW_CONFIG}"
+      rm "${NEW_CONFIG}"
       ;;
     ?)
       echo "Unsupported option -$OPTARG"
@@ -29,7 +42,10 @@ while getopts ':t:l:m' opt; do
     :)
       case "$OPTARG" in
         t)
-          bash "${LIB_PATH}/ec2-tags.sh" "${AWS_INSTANCE_ID}" ''
+          NEW_CONFIG=`mktemp -p "${CONFIG_PATH}"`
+          bash "${LIB_PATH}/ec2-tags.sh" "${AWS_INSTANCE_ID}" 'docker.' | sed 's/^/  /' >> "${NEW_CONFIG}"
+          ros config merge < "${NEW_CONFIG}"
+          rm "${NEW_CONFIG}"
           ;;
         *)
           echo "Argument required for -$OPTARG"
